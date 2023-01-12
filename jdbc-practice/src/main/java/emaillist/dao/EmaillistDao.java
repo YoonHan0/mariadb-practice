19b4d498d1aaaa57c4ede119d48c112494f1a110
YoonHan0 <dbsgksdud12@naver.com>
2023-01-12 14:56:31 +0900

    emaillist #1

---
+++ jdbc-practice/src/main/java/emaillist/dao/EmaillistDao.java
@@ -0,0 +1,14 @@
+package emaillist.dao;
+
+import java.util.List;
+
+import emaillist.vo.EmaillistVo;
+
+public class EmaillistDao {
+
+	public List<EmaillistVo> findAll() {
+		
+		return null;
+	}
+
+}
---
+++ jdbc-practice/src/main/java/emaillist/vo/EmaillistVo.java
@@ -0,0 +1,31 @@
+package emaillist.vo;
+
+public class EmaillistVo {
+	private String firstName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	private String lastName;
+	private String email;
+	public String getFristName() {
+		return firstName;
+	}
+	public void setFristName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
